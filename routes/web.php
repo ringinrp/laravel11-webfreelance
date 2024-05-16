32ee3c566844d3b82c3581501a0e94c5a4565474
ringinrp <ringinrp21@gmail.com>
2024-05-16 13:10:17 +0700

    update nav, route

--- routes/web.php
+++ routes/web.php
@@ -25,64 +25,67 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::middleware('can:withdraw wallet')->group(function(){
+    Route::middleware('can:withdraw wallet')->group(function () {
         Route::get('/dashboard/wallet', [DashboardController::class, 'wallet'])
-        ->name('dashboard.wallet');
+            ->name('dashboard.wallet');
         Route::get('/dashboard/wallet/withdraw', [DashboardController::class, 'wallet_withdraw'])
-        ->name('dashboard.wallet.withdraw');
+            ->name('dashboard.wallet.withdraw');
         Route::post('/dashboard/wallet/withdraw/store', [DashboardController::class, 'wallet_withdraw_store'])
-        ->name('dashboard.wallet.withdraw.store');
+            ->name('dashboard.wallet.withdraw.store');
     });
 
-    Route::middleware('can:topup wallet')->group(function(){
+    Route::middleware('can:topup wallet')->group(function () {
         Route::get('/dashboard/wallet/topup', [DashboardController::class, 'topup_wallet'])
-        ->name('dashboard.wallet.topup');
+            ->name('dashboard.wallet.topup');
         Route::post('/dashboard/wallet/topup/store', [DashboardController::class, 'topup_wallet_store'])
-        ->name('dashboard.wallet.topup.store');
+            ->name('dashboard.wallet.topup.store');
     });
 
-    Route::middleware('can:apply job')->group(function(){
+    Route::middleware('can:apply job')->group(function () {
         Route::get('/apply/{project:slug}', [FrontController::class, 'apply_job'])
-        ->name('front.apply_job');
+            ->name('front.apply_job');
         Route::post('/apply/{project:slug}/submit', [FrontController::class, 'apply_job_stor'])
-        ->name('front.apply_job.store');
+            ->name('front.apply_job.store');
         Route::get('/dashboard/proposals', [DashboardController::class, 'proposals'])
-        ->name('dashboard.proposals');
+            ->name('dashboard.proposals');
         Route::get('/dashboard/proposal_details/{project}/{projectApplicant}', [DashboardController::class, 'proposal_details'])
-        ->name('dashboard.proposal_details');
+            ->name('dashboard.proposal_details');
     });
 
     Route::prefix('admin')->name('admin.')->group(function () {
-        Route::middleware('can:manage wallet')->group(function(){
-            Route::get('/wallet/topups', [WalletTransactionController::class, 'wallet_topups'])
-            ->name('topups');
-            Route::get('/wallet/withdrawls', [WalletTransactionController::class, 'wallet_withdrawls'])
-            ->name('withdrawls');
+
+        Route::middleware('can:manage wallets')->group(function () {
+            Route::get('/wallet/topups', [WalletTransactionController::class, 'wallet_topups'])->name('topups');
+
+            Route::get('/wallet/withdrawals', [WalletTransactionController::class, 'wallet_withdrawals'])->name('withdrawals');
+
             Route::resource('wallet_transactions', WalletTransactionController::class);
         });
-        Route::middleware('can:manage applicants')->group(function(){
+
+        Route::middleware('can:manage applicants')->group(function () {
             Route::resource('project_applicants', ProjectApplicantController::class);
         });
-        Route::middleware('can:manage projects')->group(function(){
+
+        Route::middleware('can:manage projects')->group(function () {
             Route::resource('projects', ProjectController::class);
 
-            Route::post('/project/{ProjectApplicant}/completed', [ProjectController::class, 'complete_project_store'])
-            ->name('complete_project.store');
-            Route::get('/project/{project}/tools', [WalletTransactionController::class, 'tools'])
-            ->name('projects.tools');
-            Route::get('/project/{project}/tools/store', [WalletTransactionController::class, 'tools_store'])
-            ->name('projects.tools.store');
+            Route::post('/project/{projectApplicant}/completed', [ProjectController::class, 'complete_project_store'])->name('complete_project.store');
+
+            Route::get('/project/{project}/tools', [ProjectController::class, 'tools'])->name('projects.tools');
+
+            Route::post('/project/{project}/tools/store', [ProjectController::class, 'tools_store'])->name('projects.tools.store');
 
             Route::resource('project_tools', ProjectToolController::class);
         });
-        Route::middleware('can:manage categories')->group(function(){
-            Route::resource('catagories', CategoryController::class);
+
+        Route::middleware('can:manage categories')->group(function () {
+            Route::resource('categories', CategoryController::class);
         });
-        Route::middleware('can:manage tools')->group(function(){
+
+        Route::middleware('can:manage tools')->group(function () {
             Route::resource('tools', ToolController::class);
         });
     });
-
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
