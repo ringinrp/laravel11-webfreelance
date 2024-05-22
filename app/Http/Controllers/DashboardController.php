e821f0d179eef8814dd0d8294fa1d1b43b2e95f5
ringinrp <ringinrp21@gmail.com>
2024-05-22 08:51:15 +0700

    update wallet

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\WalletTransaction;
 
 class DashboardController extends Controller
 {
-    //
+    public function wallet(){
+        $user = Auth::user();
+
+        $wallet_transactions = WalletTransaction::where('user_id', $user->id)
+        ->orderByDesc('id')
+        ->paginate(10);
+
+        return view('dashboard.wallet', compact('wallet_transactions'));
+
+    }
 }
